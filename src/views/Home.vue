eb5a854eb14bf6270968f48d439d7b1aa62b50be
y-kawamura <tetora.y@gmail.com>
2020-03-06 10:22:43 +0900

    Add singup/login path

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="font-weight-thin display-3 my-12 text-center">Trello Clone</h1>
+    <v-btn
+      :to="{ name: 'Login' }"
+      class="my-6 mx-auto d-flex"
+      outlined
+      color="indigo"
+      max-width="350px"
+    >
+      Login
+    </v-btn>
+    <v-btn
+      :to="{ name: 'SignUp' }"
+      class="my-6 mx-auto d-flex"
+      color="indigo darken-1"
+      dark
+      max-width="350px"
+    >
+      Sign Up
+    </v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
