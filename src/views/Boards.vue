6e89df24db768b37efc03f3d8fa2f30e3cb38242
y-kawamura <tetora.y@gmail.com>
2020-03-07 11:42:12 +0900

    Add Logout and nativagation drawer view

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -1,5 +1,37 @@
 <template>
   <v-app>
+    <!-- navigation drawer -->
+    <v-navigation-drawer
+      v-model="drawer"
+      right
+      temporary
+      app
+    >
+      <v-list nav>
+        <v-list-item link>
+          <v-list-item-content>
+            <div class="avatar" @click="drawer = !drawer">
+              <v-avatar color="white" size="32">
+                <img
+                  src="@/assets/avatar.png"
+                  alt="User image"
+                >
+              </v-avatar>
+              <strong class="mx-2 font-weight-light">
+                {{ user.displayName }} @{{ user.username }}
+              </strong>
+            </div>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item link @click="logout">
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
       color="red lighten-1"
@@ -9,16 +41,19 @@
         Trello Clone
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-avatar color="white" size="32">
-        <img
-          src="@/assets/avatar.png"
-          alt="User image"
-        >
-      </v-avatar>
-      <strong class="mx-2 font-weight-light">
-        {{ user.displayName }}
-      </strong>
+      <div class="avatar" @click="drawer = !drawer">
+        <v-avatar color="white" size="32">
+          <img
+            src="@/assets/avatar.png"
+            alt="User image"
+          >
+        </v-avatar>
+        <strong class="mx-2 font-weight-light">
+          {{ user.displayName }}
+        </strong>
+      </div>
     </v-app-bar>
+
     <v-content>
       <v-container fluid>
         <h1>This is boards page</h1>
@@ -28,12 +63,35 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Boards',
+  data() {
+    return {
+      drawer: false,
+    };
+  },
   computed: {
     ...mapGetters('auth', ['user']),
   },
+  methods: {
+    ...mapActions('auth', { authLogout: 'logout' }),
+    logout() {
+      this.authLogout()
+        .then(() => {
+          this.$router.push('/login');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
 };
 </script>
+
+<style scoped>
+.avatar {
+  cursor: pointer;
+}
+</style>
