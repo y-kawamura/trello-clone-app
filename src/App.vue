723857ba0dcbea105b319729f14a67f0e1a02adb
y-kawamura <tetora.y@gmail.com>
2020-03-07 09:25:40 +0900

    Create boards header view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    </v-app-bar>
-
-    <v-content>
-      <router-view/>
-    </v-content>
+    <router-view/>
     <v-footer fixed="fixed" app>
       <span>&copy; 2020 - Made by penta</span>
     </v-footer>
--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>this is boards page</h1>
+  <v-app>
+    <v-app-bar
+      app
+      color="red lighten-1"
+      dark
+    >
+      <v-toolbar-title class="font-italic font-weight-light px-1">
+        Trello Clone
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-avatar color="white" size="32">
+        <img
+          src="@/assets/avatar.png"
+          alt="User image"
+        >
+      </v-avatar>
+      <strong class="mx-2 font-weight-light">
+        {{ user.displayName }}
+      </strong>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <h1>This is boards page</h1>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'Boards',
+  computed: {
+    ...mapGetters('auth', ['user']),
+  },
 };
 </script>
