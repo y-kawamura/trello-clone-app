679bad81c1db9193ae33eadff058b32752f4b3e5
y-kawamura <tetora.y@gmail.com>
2020-03-23 11:08:01 +0900

    Edit list name

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -4,7 +4,29 @@
     @dragover="onListDragOver($event)"
     @dragleave="onListDragLeave"
   >
-    <v-card-text class="pa-2">{{ list.name }}</v-card-text>
+    <v-card-title class="pt-2 pb-0">
+      <span
+        @click="showListForm"
+        v-if="!isShowListForm"
+      >
+        {{ list.name }}
+      </span>
+      <v-form
+        v-if="isShowListForm"
+        @submit.prevent="updateListName"
+      >
+        <v-text-field
+          v-model="newListName"
+          outlined
+          hide-details
+          dense
+          flat
+          autofocus
+          @blur="isShowListForm=false"
+        >
+        </v-text-field>
+      </v-form>
+    </v-card-title>
 
     <!-- cards -->
     <template>
@@ -34,6 +56,12 @@ import CardForm from '@/components/CardForm.vue';
 
 export default {
   name: 'ListItem',
+  data() {
+    return {
+      isShowListForm: false,
+      newListName: '',
+    };
+  },
   components: {
     CardItem,
     CardForm,
@@ -72,6 +100,16 @@ export default {
     },
     onListDragLeave() {
       this.setDroppingList(null);
+    },
+    showListForm() {
+      // set current title when edit form opened
+      this.newListName = this.list.name;
+      this.isShowListForm = true;
+    },
+    async updateListName() {
+      this.list.name = this.newListName;
+      await this.list.save();
+      this.isShowListForm = false;
     },
   },
   created() {
